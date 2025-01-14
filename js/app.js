557c1fc22eb3649bf519d36319bdecb0de6986f5
Alvaro Garcia <alvarogprod@gmail.com>
2025-01-14 21:09:42 +0100

    Landing page razer terminada

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+
+  // Selecciona todos los formularios con la clase 'needs-validation'
+  var forms = document.querySelectorAll('.needs-validation');
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  });
+
+
+
